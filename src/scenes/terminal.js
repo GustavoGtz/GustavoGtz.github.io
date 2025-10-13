32cf02f75db0c67402e5c1f11e5aeeeeaf0f5ba0
Rei <gustavogtznav@gmail.com>
2025-10-12 21:21:12 -0700

    Terminal finished, only left the details

--- src/scenes/terminal.js
+++ src/scenes/terminal.js
@@ -8,102 +8,56 @@ export default class Terminal extends Phaser.Scene {
     super('Terminal');
   }
 
- preload() {
-    const fontKey = 'c64';
-    this.load.bitmapFont(fontKey, fontPng, fontXml);
-}
-
+  preload() {
+    this.load.bitmapFont('c64', fontPng, fontXml);
+  }
 
   create() {
     this.cameras.main.setBackgroundColor('#50459b');
     this.textColor = '#887ecb';
     this.fontSize = 20;
+    this.verticalSpace = 20;
     
-    this.welcomeText = '64K RAM SYSTEM 38911 BASIC BYTES'
-    this.add.bitmapText(0, 0 , 'c64', this.welcomeText, 20);
-
-    
-    /*
-   this.textLines = [
-      '**** FIRMIN 64 BASIC V2 ****',
-      '64K RAM SYSTEM  38911 BASIC BYTES FREE',
+    this.initialText = [
+      'COMMODORE 64 BASIC V2',
+      '64K RAM SYSTEM 38911 BASIC BYTES FREE',
       '',
-      'TYPE START() OR PRESS SPACE TO RUN'
+      ' FIRMIN IN THE CITY',
+      '  * GUSTAVO GUTIERREZ PORTFOLIO',
+      '  * 2025',
+      '',
+      ' PRESS SPACE BAR TO START'
     ];
+    for (let i = 0; i < this.initialText.length; i++) {
+      this.add.bitmapText(0, i*this.verticalSpace, 'c64', this.initialText[i], this.fontSize);
+    }
 
-    let testtext = this.displayText = this.add.text(20, 40, '', {
-      fontFamily: this.fontFam,
-      fontSize: this.fontSize,
-      color: this.textColor,
-      lineSpacing: 8
-    });
-    testtext.setResolution(1);
-
-    this.cursor = this.add.text(20, 150, '█', {
-      fontFamily: 'Courier',
-      fontSize: 14,
-      color: this.textColor
-    }); */
-
-    
-
+    this.cursorTimer = 0;
     this.cursorVisible = true;
-    this.inputBuffer = '';
-
-    this.lineIndex = 1;
-    this.typeNextLine();
-
-    // Blinking cursor
-    this.time.addEvent({
-      delay: 500,
-      loop: true,
-      callback: () => {
-        this.cursorVisible = !this.cursorVisible;
-        this.cursor.setVisible(this.cursorVisible);
-      }
-    });
-
-    this.input.keyboard.on('keydown', this.handleInput, this);
-  }
-
-  typeNextLine() {
-    if (this.lineIndex < this.textLines.length) {
-      this.displayText.text += this.textLines[this.lineIndex] + '\n';
-      this.lineIndex++;
-      this.time.delayedCall(400, this.typeNextLine, [], this);
-    } else {
-      this.displayText.text += '\nREADY.\n';
-    }
-  }
-
-  handleInput(e) {
-    if (e.key === ' ') {
-      this.startGame();
-    } else if (e.key === 'Enter') {
-      if (this.inputBuffer.trim().toLowerCase() === 'start()') {
-        this.startGame();
-      } else {
-        this.displayText.text += '\nSYNTAX ERROR\nREADY.\n';
-        this.inputBuffer = '';
-      }
-    } else if (e.key === 'Backspace') {
-      this.inputBuffer = this.inputBuffer.slice(0, -1);
-      this.updatePrompt();
-    } else if (e.key.length === 1) {
-      this.inputBuffer += e.key;
-      this.updatePrompt();
-    }
-  }
-
-  updatePrompt() {
-    this.prompt?.destroy();
-    this.prompt = this.add.text(
-      20,
-      this.displayText.y + this.displayText.height,
-      `READY. ${this.inputBuffer}`,
-      { fontFamily: 'Courier', fontSize: 14, color: this.textColor }
+    this.blinkingCursor = this.add.text(
+        0,
+        this.verticalSpace * this.initialText.length, // Y position below last line
+        '  █',
+        { 
+            font: this.fontSize + 'px Courier',
+            fill: this.textColor                
+        }
     );
+
+    this.input.keyboard.on('keydown-SPACE', (event) => {
+      this.startGame();
+    });
   }
+
+  update(time, delta) {
+
+    this.cursorTimer += delta;
+    if (this.cursorTimer > 500) {
+        this.cursorTimer = 0;
+        this.blinkingCursor.setVisible(!this.blinkingCursor.visible);
+    }
+}
+
 
   startGame() {
     this.scene.start('GameScene');
